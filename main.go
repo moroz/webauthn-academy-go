9887b4f27a37b959b614b9f47271aea87dc913f7
Karol Moroz <karol@moroz.dev>
2024-05-11 18:43:19 +0800

    Add migration to create users table

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	db := sqlx.MustConnect("postgres", "postgres://postgres:postgres@localhost/academy_dev?sslmode=disable")
+	row := db.QueryRowx("select 2 + 2")
+	var value int
+	err := row.Scan(&value)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(value)
 }
