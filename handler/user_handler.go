181ac64acaa7fcbfed2454a276bc71488379465e
Karol Moroz <karol@moroz.dev>
2024-05-20 20:16:18 +0800

    Demonstrate unstyled sign up page

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gookit/validate"
 	"github.com/jmoiron/sqlx"
 	"github.com/moroz/webauthn-academy-go/service"
 	"github.com/moroz/webauthn-academy-go/templates/users"
@@ -19,42 +18,9 @@ func UserHandler(db *sqlx.DB) userHandler {
 	return userHandler{service.NewUserService(db)}
 }
 
-type usersNewAssigns struct {
-	RequestContext
-	Params types.NewUserParams
-	Errors validate.Errors
-}
-
 func (h *userHandler) New(w http.ResponseWriter, r *http.Request) {
 	err := users.New(types.NewUserParams{}, nil).Render(r.Context(), w)
 	if err != nil {
-		log.Print(err)
+		log.Printf("Rendering error: %s", err)
 	}
-}
-
-func (h *userHandler) Create(w http.ResponseWriter, r *http.Request) {
-	var params types.NewUserParams
-	err := decoder.Decode(&params, r.PostForm)
-	if err != nil {
-		handleError(w, http.StatusBadRequest)
-		return
-	}
-
-	_, err, validationErrors := h.us.RegisterUser(params)
-
-	if err != nil || validationErrors != nil {
-		addFlash(r, w, types.FlashMessage{
-			Severity: types.FlashMessageSeverity_Error,
-			Content:  "An error occurred, please review:",
-		})
-
-		users.New(params, validationErrors).Render(r.Context(), w)
-		err := users.New(types.NewUserParams{}, nil).Render(r.Context(), w)
-		if err != nil {
-			log.Print(err)
-		}
-		return
-	}
-
-	http.Redirect(w, r, "/sign-in", http.StatusMovedPermanently)
 }
--- main.go
+++ main.go
@@ -3,34 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	"github.com/moroz/webauthn-academy-go/config"
 	"github.com/moroz/webauthn-academy-go/handler"
 )
 
 func main() {
-	db := sqlx.MustConnect("postgres", config.DatabaseURL)
+	db := sqlx.MustConnect("postgres", os.Getenv("DATABASE_URL"))
 
 	r := chi.NewRouter()
-
 	r.Use(middleware.Logger)
-	r.Use(handler.FetchSession)
-	r.Use(handler.FetchFlash)
 
 	users := handler.UserHandler(db)
 	r.Get("/", users.New)
-
-	sessions := handler.SessionHandler(db)
-	r.Get("/sign-in", sessions.New)
-
-	r.Group(func(r chi.Router) {
-		r.Use(handler.ParseForm)
-		r.Post("/users/register", users.Create)
-	})
 
 	log.Println("Listening on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", r))
